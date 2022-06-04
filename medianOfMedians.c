d7f24a689fc08bb73458dc35b972322cd8d687cd
antiloplastico <albertorob98@gmail.com>
2022-06-04 22:32:10 +0200

    Update medianOfMedians.c
    Finally a totally working code

--- medianOfMedians.c
+++ medianOfMedians.c
@@ -54,13 +54,13 @@ int partition3(int *a, int i, int j){
 			swapp (&(a[k+1]), &(a[l]));
 			k++;
 		} else {//l > pivot
-			//non faccio niente
+			//nothing to do
 		}
 	}
 	return k;
 }
 
-void quick3(int *a, int i, int j){ //vettore in 3 parti: a e' il vettore stesso, j fine scansione, i inizio scansione
+void quick3(int *a, int i, int j){
 	if (i < j){
 		int c = partition3(a, i, j);
 			quick3(a, i, c);
@@ -69,12 +69,8 @@ void quick3(int *a, int i, int j){ //vettore in 3 parti: a e' il vettore stesso,
 }
 
 int medianOfMedians (int *a, int s, int f){
-	printf("\nArrivo: ");
-	for (int z=s; z<=f; z++){
-		printf("%d ", a[z]);
-	}
 	if (f-s > 1){
-		int suppl = (f-s)/5+1; //quanti pezzi
+		int suppl = (f-s)/5+1; //how many pieces
 		int i;
 		
 		for (i=s; i+4<=f; i+=5){
@@ -94,7 +90,6 @@ int medianOfMedians (int *a, int s, int f){
 		if (check != suppl){
 			swapp(&a[s+suppl-1], &a[(rem+f+1)/2]);
 		}
-		printf("finale: s:%d, suppl:%d\n", s, suppl);
 		return medianOfMedians(a, s, s+suppl-1);
 	} else {
 		if (f-s == 1 && a[s] < a[f]){
@@ -105,29 +100,25 @@ int medianOfMedians (int *a, int s, int f){
 	}
 }
 
-int partition (int arr[], int pivot, int low, int high)
-{
-    int i = low; // Index of smaller element and indicates the right position of pivot found so far
- 
-    for (int j = low; j <= high; j++)
-    {
-        // If current element is smaller than the pivot
-        if (arr[j] < pivot)
-        {
-			swapp(&arr[i], &arr[j]);
-            i++; // increment index of smaller element
-        }
-    }
-    //swapp(&arr[i + 1], &arr[high]);
-    return i;
+int partition(int *a, int pivot, int i, int j){
+	int k = i;
+	for (int l=i+1; l<=j; l++){
+		if (a[l] < pivot) {
+			swapp (&(a[k]), &(a[l]));
+			swapp (&(a[k+1]), &(a[l]));
+			k++;
+		} else {//l > pivot
+			//nothing to do
+		}
+	}
+	return k+1;
 }
 
 
 int main() {
-	int a[1000];
-	int len = scanArray(a); //7 3 11 5
-	//int a[]={21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
-	//int len=sizeof(a)/sizeof(int);
+	//int a[10000];
+	int* a = (int *) malloc(sizeof(int) * 10000);
+	int len = scanArray(a);
 	int nth; //2
 	scanf ("%d", &nth);
 	
@@ -135,33 +126,15 @@ int main() {
 	int p=0;
 	int q=len-1;
 	int pvt=-1;
-	
-	//printf("%d\n", mMedian);
-	/*for (int i=0; i<len; i++){
-		printf("Array %d: ", i+1);
-		mMedian=medianOfMedians(a, i, q);
-		//pvt=partition(a, i+1, p, q);
-		for (int z=i; z<len; z++){
-			//printf ("%d ",a[z]);
-		}
-		printf(" pvt:%d\n", mMedian);
-	}*/
-	//for (nth=1; nth<=len; nth++){
-	while (nth-1 != pvt){ //3-0>2-1
-	//int polo=10;
-	//while (polo>0){
+
+	while (q-p>0){
 		mMedian=medianOfMedians(a, p, q);
-		pvt=partition(a, mMedian, p, q);//dalla posizione pvt in poi sono >=
-		printf("%d %d\n", pvt, nth-1);
-		for (int z=p; z<=q; z++)
-			printf("%d ", a[z]);
-		printf("\n");
-		if (nth-1 < pvt){ //7<3
+		pvt=partition(a, mMedian, p, q);//from pvt ahead, numbers are greater or equal
+		if (nth-1 < pvt){
 			q=pvt-1;
-		} else {
+		} else{
 			p=pvt;
 		}
-		//polo--;
 	}
 	printf("%d ", a[nth-1]);
 
